468ea37133bdb83310a74435027cffde93aee90b
DucCuong2412 <162032280+DucCuong2412@users.noreply.github.com>
2025-04-10 22:26:08 +0700

    sudmid chat

---
+++ Assets/scripts/ChatUI/ChatManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Fusion;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ChatManager : NetworkBehaviour
+{
+    public static ChatManager instance;
+    private List<string> chatMessage= new List<string>();   
+    public ChatUI ChatUI;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    [Rpc(RpcSources.All,RpcTargets.All)]
+    public void RpcReceiveChatMessage(string playername,string message)
+    {
+        string formatedMessage=$"{playername} :{message}";   
+        chatMessage.Add(formatedMessage);
+        ChatUI.ChatContent.text += formatedMessage + "/n";
+
+    }
+    public void SendChatMessage(string message)
+    {
+        string playername=Runner.LocalPlayer.PlayerId.ToString();
+        RpcReceiveChatMessage(playername,message);
+        Debug.Log($"{playername} :{message}");
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/ChatUI/ChatUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatUI : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    public Button SendButton;
+    public TextMeshProUGUI ChatContent;
+    private void Start()
+    {
+        SendButton.onClick.AddListener(SendMessage);
+        
+    }
+    private void SendMessage()
+    {
+        string message=inputField.text;
+        if(!string.IsNullOrEmpty(message))
+        {
+            //gọi chat managager
+            ChatManager.instance.SendChatMessage(message);
+            inputField.text = "";//xóa nội dung khi đã được gửi đi
+
+        }
+    }
+}
