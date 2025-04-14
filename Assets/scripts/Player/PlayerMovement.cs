e6cbc52e7c14f344fac69a7003cfc4f37a2e8a2f
DucCuong2412 <162032280+DucCuong2412@users.noreply.github.com>
2025-04-14 23:42:14 +0700

    submid

--- Assets/scripts/Player/PlayerMovement.cs
+++ Assets/scripts/Player/PlayerMovement.cs
@@ -1,4 +1,4 @@
-using Fusion;
+﻿using Fusion;
 using UnityEngine;
 
 public class PlayerMovement : NetworkBehaviour
@@ -7,51 +7,34 @@ public class PlayerMovement : NetworkBehaviour
     public Animator animator;
     public int speedRotate = 10;
     private Vector3 move;
-    public RoomManager roomManager;
-
-      public  bool isStartGame = false;
-
-
     public float speed = 5f;
+
+    private RoomManager roomManager;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         roomManager = FindAnyObjectByType<RoomManager>();
+    }
 
-    }
-    private void Update()
-    {
-        Debug.Log(roomManager.isStartGame);
-        if (roomManager.isStartGame)
-        {
-            isStartGame = true;
-        }
-        else
-        {
-            isStartGame = false;
-        }
-        
-    }
     public override void FixedUpdateNetwork()
     {
-        
-        if (!Object.HasInputAuthority) return;
-        if (isStartGame )
+        if (!Object.HasInputAuthority || roomManager == null)
+            return;
+
+        // Di chuyển khi GameStarted = true
+        if (roomManager.GameStarted)
         {
             Move();
-
         }
-
     }
+
     private void Move()
     {
-
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         move = new Vector3(horizontal, 0, vertical);
         PlayerMoving();
-
     }
 
     private void PlayerMoving()
@@ -59,13 +42,8 @@ public class PlayerMovement : NetworkBehaviour
         if (move.sqrMagnitude >= 0.001f)
         {
             Quaternion targetRotation = Quaternion.LookRotation(move);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10 * Runner.DeltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speedRotate * Runner.DeltaTime);
         }
         controller.Move(move * speed * Runner.DeltaTime);
     }
-
-
-
-
 }
-
